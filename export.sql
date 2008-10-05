d1c6c3d50d3d813257554fd71e03d68309fbcae6
dhavaltw <dhavaltw@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-10-05 07:41:40 +0000

    added sql file for taking a vote count and points message in csv forgot saving the file before :)-- dhaval
    git-svn-id: http://svn.assembla.com/svn/orter/retro@52 e0e46c49-be69-4f5a-ad62-21024a331aea

--- export.sql
+++ export.sql
@@ -1,1 +1,1 @@
-SELECT message,count(votes.point_id) INTO OUTFILE '/tmp/result.text' FIELDS TERMINATED BY ','  FROM votes,points WHERE points.id = votes.point_id GROUP BY votes.point_id;
+SELECT message,count(votes.point_id) INTO OUTFILE '/tmp/result.text' FIELDS TERMINATED BY ','  FROM votes,points WHERE points.id = votes.point_id GROUP BY votes.point_id; 
