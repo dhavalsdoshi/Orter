51a7b120b0115821e8704ded4acdee874a6f13b2
anay <anay@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-14 09:02:45 +0000

    <Anay/Dhaval> Yippee! Sections are now flexible but er... restricted to max of three from the create retro screen.
    git-svn-id: http://svn.assembla.com/svn/orter/retro@78 e0e46c49-be69-4f5a-ad62-21024a331aea

--- app/controllers/deeplinks_controller.rb
+++ app/controllers/deeplinks_controller.rb
@@ -1,7 +1,5 @@
 class DeeplinksController < ApplicationController
   def index
-    parameters = RetroDeepLink.decode(params[:id])
-    @retrospective_id = parameters[:retro_id]
-    @participant_code = parameters[:participant_code]
+    @retrospective_id = params[:id]
   end
 end
