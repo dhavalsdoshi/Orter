63d66f7024a4d8d4244b939f07390cb90406e293
dhavaltw <dhavaltw@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-07-19 03:13:27 +0000

    Dhaval| Putting in basic google analytics code
    git-svn-id: http://svn.assembla.com/svn/orter/retro@165 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ public/js/analytics.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    try{
+    var pageTracker = _gat._getTracker("UA-12571339-1");
+    pageTracker._setAllowLinker(true);
+    pageTracker._trackPageview();
+    }
+    catch(e){
+        
+    }
+});
