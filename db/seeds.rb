610184f9c505866e72418b650655583119f75fae
tinygrasshopper <jsnaik@gmail.com>
2011-09-11 17:51:19 +0530

    Adding seed data for a test board

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+if Retro.find_by_name("test").nil?
+  p "Creating a test retro"
+  retro = Retro.create!(:name => "test", :description => "test")
+  Section.create!(:name => "Went Well", :retro => retro)
+  Section.create!(:name => "Didn't Go Well", :retro => retro)
+  Section.create!(:name => "Action Items", :retro => retro)
+end
