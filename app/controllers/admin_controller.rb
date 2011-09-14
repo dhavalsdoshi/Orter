62c320450e64d9e735c47ddefde89aa34a3882fc
dhaval <dhaval.doshi@gmail.com>
2011-09-14 10:20:16 +0530

    making admin soft delete of points to work

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,18 +1,19 @@
 class AdminController < ApplicationController
 
   def deleted_points
-    retro = Retro.find_by_name(params[:retro_id]) unless params[:retro_id].nil?
-	@points = []
-	retro.sections.each do |s| 
-		@points.push Point::Deleted.find_by_section_id(s.id) 
-	end
+    unless params[:retro_id].nil?
+      retro = Retro.find_by_id_and_name(params[:retro_id],params[:retro_name], :include => :sections)
+      @points = []
+      retro.sections.each do |s|
+        @points.push Point::Deleted.find_by_section_id(s.id)
+      end
+    end
   end
 
   def restore_deleted
     point = Point::Deleted.find(params[:point_id])
-	p point
-	point.undestroy!
-	redirect_to :action => :deleted_points
+	  point.undestroy!
+	  redirect_to :action => :deleted_points
   end
   
 end
