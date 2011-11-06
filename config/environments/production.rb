9fa2797755c39f662a694d83fe53d1cecfa5f49a
dhaval <dhaval.doshi@gmail.com>
2011-11-06 10:01:30 +0530

    making prod log to warn level and target for links to _blank

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@ config.cache_classes = true
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
+config.log_level = :warn
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
