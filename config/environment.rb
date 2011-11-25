76f9108cabf7065c15c1499a7c77aa6fc548566e
dhaval <dhaval.doshi@gmail.com>
2011-11-25 09:08:22 +0530

    changing session_key to key for rails warning

--- config/environment.rb
+++ config/environment.rb
@@ -37,7 +37,7 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_retro_session',
+    :key => '_retro_session',
     :secret      => 'a78a9dda255b5c01087ac7c2cbdba1dd63c3cfe4941ecfac53ef995ad2fd7966982c4a17e73c50c546fc2083319feac0ec1090b4ef619b12c6c8cc3a32353f05'
   }
 
