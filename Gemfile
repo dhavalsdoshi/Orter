0c0fac95cb4a23fc6a5d30200726e1aaaabe6601
Atreyee <atreyeem@thoughtworks.com>
2012-03-23 20:38:57 +0530

    Atreyee - upgraded prawn gem and added prawnto gem to make pdf functionality work with rails 3

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ gem 'rails', '3.2.2'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem "mysql2"
-gem 'prawn', :git => "git://github.com/sandal/prawn.git", :tag => '0.10.2', :submodules => true
+gem 'prawn'
+gem 'prawnto'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/retros_controller.rb
+++ app/controllers/retros_controller.rb
@@ -35,10 +35,10 @@ class RetrosController < ApplicationController
     @retro = Retro.find(params[:id])
     headers["Content-Disposition"] = "attachment; filename=\"#{@retro.name}.#{params[:format]}\""
     respond_to do|format|
-      format.pdf {render :layout =>false} if params[:format] == 'pdf'
-      format.xls {render :layout =>false} if params[:format] == 'xls'
+      format.pdf {render :layout =>false}
+      format.xls {render :layout =>false}
     end
   end
-  
-  
+
+
 end
