b5591b008712e865a763a67550b50b151cde5a77
Dhaval <dhaval.doshi@gmail.com>
2012-05-21 19:38:09 +0530

    Atreyee - modifications to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-if Retro.find_by_name("test").nil?
+unless Retro.find_by_name("test")
   p "Creating a test retro"
   retro = Retro.create!(:name => "test", :description => "test")
-  Section.create!(:name => "Went Well", :retro => retro)
-  Section.create!(:name => "Didn't Go Well", :retro => retro)
-  Section.create!(:name => "Action Items", :retro => retro)
+  ["Went Well", "Didn't Go Well", "Action Items"].each { |name| Section.create!(:name => name, :retro => retro )}
+end
+
+unless Retro.find_by_name("feedback")
+  p "Creating a feedback retro"
+  retro = Retro.create!(:name => "feedback", :description => "feedback")
+  ["Good features", "Features to improve", "New features"].each { |name| Section.create!(:name => name, :retro => retro )}
 end
