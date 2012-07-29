7900c817414bbc1bf1d3c576f71d3bc03daa2292
Anand Agrawal <aagrawal@thoughtworks.com>
2012-07-29 11:52:46 +0530

    refactoring

--- app/controllers/retros_controller.rb
+++ app/controllers/retros_controller.rb
@@ -4,25 +4,25 @@ class RetrosController < ApplicationController
   end
 
   def create
-    @retro = Retro.new({:name =>CGI.escapeHTML(params[:name]), :description =>CGI.escapeHTML(params[:description])})
+    @retro = Retro.new({:name => CGI.escapeHTML(params[:name]), :description => CGI.escapeHTML(params[:description])})
 
     params[:numberOfSections].to_i.times do |section_number|
       section_name = ("sectionname"+section_number.to_s)
-      @retro.sections << Section.new({:name =>CGI.escapeHTML(params[section_name])})
+      @retro.sections << Section.new({:name => CGI.escapeHTML(params[section_name])})
     end
     @retro.users = [current_user] if current_user
 
     if @retro.save
       flash[:notice] = 'Retro was successfully created.'
       LOGGER.info("retro created for: {#{@retro.name}} #{@retro.id.to_s}")
-      redirect_to :controller => :retros, :action => :show, :id=>@retro.id.to_s,:name=> @retro.name
+      redirect_to :controller => :retros, :action => :show, :id => @retro.id.to_s, :name => @retro.name
     else
       render new
     end
   end
 
   def show
-    @retrospective = Retro.find_by_id_and_name(params[:id],params[:name], :include => :sections)
+    @retrospective = Retro.find_by_id_and_name(params[:id], params[:name], :include => :sections)
     render :action => :show
   end
 
@@ -34,11 +34,11 @@ class RetrosController < ApplicationController
   def export
     @retro = Retro.find(params[:id])
     headers["Content-Disposition"] = "attachment; filename=\"#{@retro.name}.#{params[:format]}\""
-    respond_to do|format|
-      format.pdf {render :layout =>false} if params[:format] == 'pdf'
-      format.xls {render :layout =>false} if params[:format] == 'xls'
+    respond_to do |format|
+      format.pdf { render :layout => false } if params[:format] == 'pdf'
+      format.xls { render :layout => false } if params[:format] == 'xls'
     end
   end
-  
-  
+
+
 end
