7574988855d9347dafbdc12e4af1abf1142b972a
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-10-26 00:16:05 +0530

    Support for 10 sections
    Also I changed the code that handles sections, so that
    potentially you can add support for any number of sections.

--- app/helpers/deeplinks_helper.rb
+++ app/helpers/deeplinks_helper.rb
@@ -1,6 +1,6 @@
 module DeeplinksHelper
   def get_section_width(number_of_sections_in_row)
-    class_name_for = {1 => "full", 2=> "half", 3 => "oneThird"}
+    class_name_for = {1 => "full", 2 => "half", 3 => "oneThird"}
     class_name_for[number_of_sections_in_row]
   end
 
@@ -10,9 +10,8 @@ module DeeplinksHelper
   end
 
   def get_rows_for_sections(sections)
-    return sections, [] if sections.size <= 3
-    return sections.slice(0..1), sections.slice(2..3) if sections.size == 4
-    return sections.slice(0..2), sections.slice(3, sections.size-1)
+    rows = sections.each_slice(3).to_a
+    return rows
   end
 
 end
--- test/unit/deeplinks_helper_test.rb
+++ test/unit/deeplinks_helper_test.rb
@@ -4,26 +4,24 @@ class DeeplinksHelperTest < ActiveSupport::TestCase
   include DeeplinksHelper
 
   def test_get_rows_for_sections_when_no_sections
-    row_one, row_two = get_rows_for_sections([])
-    assert row_one == []
-    assert row_two == []
+    rows = get_rows_for_sections([])
+    assert_equal [], rows
   end
 
   def test_get_rows_for_sections_when_one_row
-    row_one, row_two = get_rows_for_sections([1, 2, 3])
-    assert row_one == [1, 2, 3]
-    assert row_two == []
+    row_one, _ = get_rows_for_sections([1, 2, 3])
+    assert_equal [1, 2, 3], row_one
   end
 
   def test_get_rows_for_sections_when_two_rows
     row_one, row_two = get_rows_for_sections([1, 2, 3, 4, 5, 6])
-    assert row_one == [1, 2, 3]
-    assert row_two == [4, 5, 6]
+    assert_equal [1, 2, 3], row_one
+    assert_equal [4, 5, 6], row_two
   end
 
   def test_get_rows_for_four_sections
     row_one, row_two = get_rows_for_sections([1, 2, 3, 4])
-    assert row_one == [1, 2]
-    assert row_two == [3, 4]
+    assert row_one == [1, 2, 3]
+    assert row_two == [4]
   end
 end
