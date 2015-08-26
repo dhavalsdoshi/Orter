f3c4df6c3873819859c740ea53fa17673cc2a12c
Dhaval <dhaval.doshi@gmail.com>
2015-08-26 07:10:57 +0530

    fixing quot encoding in section name and sticky text

--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -1,7 +1,7 @@
 jQuery.extend( {
     isHtmlEncoded: function(str) {
         if(str == null) { return false; }
-        if(str.search(/&amp;/g) != -1 || str.search(/&lt;/g) != -1 || str.search(/&gt;/g) != -1)
+        if(str.search(/&amp;/g) != -1 || str.search(/&lt;/g) != -1 || str.search(/&gt;/g) != -1|| str.search(/&#39;/g) != -1)
             return true;
         else
             return false;
@@ -9,13 +9,13 @@ jQuery.extend( {
 
     htmlDecode: function(str) {
         if($.isHtmlEncoded(str))
-            return str.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+            return str.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&#39;/g, '\'');
         return str;
     },
 
     htmlEncode: function(str) {
         if(!$.isHtmlEncoded(str))
-            return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+            return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/'/g, '&#39;');
         return str;
     }
 });
