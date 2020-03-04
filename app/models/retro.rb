7f1193b1ad25294871637033048615795a846624
Anand Agrawal <anand.agrawal84@gmail.com>
2020-03-04 15:13:07 +0530

    fix issue related to . in name

--- app/models/retro.rb
+++ app/models/retro.rb
@@ -1,5 +1,6 @@
 class Retro < ActiveRecord::Base
   has_many :sections
   has_many :points, :through => :sections
+  validates_format_of :name, without: /\./i, message: "You cannot include dot(.) in the Retro's Name."
   has_and_belongs_to_many :users
 end
