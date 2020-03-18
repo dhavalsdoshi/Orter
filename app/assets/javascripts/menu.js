423d17f8542eb137605ce60b9b69c344e3bf42bf
Anand Agrawal <anand.agrawal84@gmail.com>
2020-03-18 23:04:32 +0530

    track board delete event

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -2,6 +2,7 @@ $(".removeBoardFromFav").on("click", function () {
   var boardName  = this.getAttribute("data-board-name"),
       boardId = this.getAttribute("data-board-id"),
       element =  this;
+  $a.trackEvent('board', 'delete', boardId);
   if (confirm("Are you sure you want to delete " + boardName + " from favorite boards?")) {
     $.ajax({
       url: "/retros/" + boardId + "/remove_from_my_board.json",
